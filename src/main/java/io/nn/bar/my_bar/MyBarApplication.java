3baa4920ae26960289795497d3cbf5e85dfcb968
Danil-Malkin-NN <danilmalkin2015@gmail.com>
2023-07-10 19:51:29 +0300

    Начало проекта с кирилом

---
+++ src/main/java/io/nn/bar/my_bar/MyBarApplication.java
@@ -0,0 +1,13 @@
+package io.nn.bar.my_bar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/nn/bar/my_bar/MyBarApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nn.bar.my_bar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
